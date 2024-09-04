9d7ff0b87b94e3581da5ddffdc9095fdebe5d680
wsmarlonws <123284024+wsmarlonws@users.noreply.github.com>
2024-09-04 15:12:41 -0300

    Delete backend directory

--- backend/build-and-run-docker-sql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-COMPOSE_FILE="-f docker-compose.databases.yml"
-export $(grep -v '^#' .env | xargs)
-docker compose $COMPOSE_FILE build --no-cache
-docker compose $COMPOSE_FILE up $SERVICES_TO_COMPOSE_UP_SQL "$@"
